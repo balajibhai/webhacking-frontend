db48b52526475471deab2f9627ecc1ac2287f638
Balaji Narasimhan <55449960+balajibhai@users.noreply.github.com>
2023-08-09 21:56:47 +0530

    register init

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import TextField from "./TextField";
+import bcrypt from "bcryptjs";
 
 interface MeProps {
   reg: boolean;
@@ -11,6 +12,8 @@ const Register = ({ reg }: MeProps) => {
     password: "",
     confirm: "",
   });
+  const [submitted, setSubmitted] = useState(false);
+
   const handleRegProcess = (value?: string, name?: string) => {
     if (name) {
       setRegProcess((prevstate) => ({
@@ -19,28 +22,51 @@ const Register = ({ reg }: MeProps) => {
       }));
     }
   };
+
+  const handleHashing = async () => {
+    try {
+      const newPassword = await bcrypt.hash(regProcess["password"], 10);
+      const confirmPassword = await bcrypt.hash(regProcess["password"], 10);
+      const isMatch = await bcrypt.compare(newPassword, confirmPassword);
+      if (isMatch) {
+        console.log("The passwords are same");
+        return newPassword;
+      }
+      console.log("The passwords do not match");
+      return null;
+    } catch (error) {
+      console.error("Error hashing the password", error);
+      return null;
+    }
+  };
+
+  const handleSubmit = () => {
+    handleHashing();
+    setSubmitted(true);
+  };
+
   if (reg) {
     return (
       <div>
         <TextField
-          value={regProcess["email"]}
+          value={submitted ? "" : regProcess["email"]}
           placeholder="Enter email"
           onChange={handleRegProcess}
           name="email"
         />
         <TextField
-          value={regProcess["password"]}
+          value={submitted ? "" : regProcess["password"]}
           placeholder="Password"
           onChange={handleRegProcess}
           name="password"
         />
         <TextField
-          value={regProcess["confirm"]}
+          value={submitted ? "" : regProcess["confirm"]}
           placeholder="Confirm Password"
           onChange={handleRegProcess}
           name="confirm"
         />
-        <button>Submit</button>
+        <button onClick={handleSubmit}>Submit</button>
       </div>
     );
   }
